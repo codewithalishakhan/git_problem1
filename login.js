2b19e66327d22d9b4584520a03d89972b2458c72
Alisha Khan <alishakhannahid@gmail.com>
2022-11-30 19:47:39 +0530

    login problem

---
+++ login.js
@@ -0,0 +1,32 @@
+for prime//
+
+
+let num=10;
+let bag=0;
+for (let i=1;i<num;i++){
+    if (num%i==0){
+        bag++;
+    }
+}
+if(bag==0){
+    console.log("Yes")
+}else{
+        console.log("No")
+    }
+
+
+
+
+for palindrome//
+
+
+a=str.split("");
+b=a.reverse("");
+c=b.join("");
+
+
+if(str==b){
+console.log("Yes");
+}else{
+console.log("No");
+}
